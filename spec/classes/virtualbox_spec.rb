41e5391329e022f7ebe35c85cee3fa34449340c2
William Tsoi <william@williamtsoi.net>
2015-04-16 12:22:18 +1000

    updated spec

--- spec/classes/virtualbox_spec.rb
+++ spec/classes/virtualbox_spec.rb
@@ -9,9 +9,9 @@ describe 'virtualbox' do
     })
   end
   it do
-    should contain_package('VirtualBox-4.3.14-95030').with({
+    should contain_package('VirtualBox-4.3.26-98988').with({
       :ensure   => 'installed',
-      :source   => 'http://download.virtualbox.org/virtualbox/4.3.14/VirtualBox-4.3.14-95030-OSX.dmg',
+      :source   => 'http://download.virtualbox.org/virtualbox/4.3.26/VirtualBox-4.3.26-98988-OSX.dmg',
       :provider => 'pkgdmg',
       :require  => 'Exec[Kill Virtual Box Processes]',
     })
